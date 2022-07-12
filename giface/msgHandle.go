9aeede4627ad00c38baa1290d5efd20559356f8b
cexll <evanxian9@gmail.com>
2022-07-12 10:22:26 +0800

    feat: msgHandler

---
+++ giface/msgHandle.go
@@ -0,0 +1,8 @@
+package giface
+
+type MsgHandle interface {
+	DoMsgHandler(request Request)          //马上以非阻塞方式处理消息
+	AddRouter(msgID uint32, router Router) //为消息添加具体的处理逻辑
+	StartWorkerPool()                      //启动worker工作池
+	SendMsgToTaskQueue(request Request)    //将消息交给TaskQueue,由worker进行处理
+}
